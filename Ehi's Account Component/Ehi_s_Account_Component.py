99262c0fa2b81a99f99d4357624dfcc37d3922f6
Osezuah <tessosezuah@gmail.com>
2023-10-22 16:14:36 -0400

    Ehi - Cleaaned up the code and added comments

--- Ehi's Account Component/Ehi_s_Account_Component.py
+++ Ehi's Account Component/Ehi_s_Account_Component.py
@@ -1,34 +1,26 @@
-
+#information class
 class information:
-    def __init__(self,name, email, password):
-        self.name = name
+    def __init__(self,name, email, password): #initializing components
+        self.name = name 
         self.email = email
         self.password = password
     def __str__(self):
         return f"{self.name} {self.email} {self.password}"
-    def storeData (self):
+    def storeData (self): #to store the name, email and password in list
         storage.append(self)
         
-
+# a single user account that inherits information class 
 class singleUser(information):
    pass
 
-  
 # information
-storage = []
+storage = [] #empty list that stores everything
+#initializing strings that hold the information
 username = ""
 userEmail = ""
 userPassword = ""
 user1 = singleUser(username, userEmail, userPassword)
     
 user1.storeData()
-for val in storage:
+for val in storage: #adds the information in the list
     print(val.name, val.email, val.password)
-# username1 = input("Enter your username: ")
-# userEmail1 = input("Enter your email: ")
-# userPassword1 = input("Enter your password: ")
-
-# user2 = singleUser(username1, userEmail1, userPassword1)
-# print(user2)
-# data1 = user2.storeData()
-# print(data1)
